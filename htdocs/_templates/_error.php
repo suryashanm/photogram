202ba031639e83e2840821a4a9342fd09d4e89e4
suryashanm <shanmsurya@gmail.com>
2024-03-06 08:42:30 +0000

    404

--- htdocs/_templates/_error.php
+++ htdocs/_templates/_error.php
@@ -1,8 +1,9 @@
-<main class="flex-shrink-0">
-	<div class="container">
-		<h1 class="mt-5">Error!</h1>
-		<p class="lead">The page that you are trying to load is not available.
+<main class="flex-shrink-0 text-center">
+	<div class="container mt-5 pt-5">
+		<h1 class="mt-5 text-danger">404</h1>
+		<h4>Error - There's nothing here...</h4>
+		<p class="text-muted">..Maybe the page you're looking for is not found or never existed. 
 		</p>
-		<p>Go home <a href="/"></p>
+		<button class="rounded-pill border-0 bg-primary px-3 py-2"><a href="/" class="text-decoration-none text-white">Go to home</a></button>
 	</div>
 </main>
